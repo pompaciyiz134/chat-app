009c229f022bcfbb01583e8eeeb49f715ce8b6db
pompaciyiz134 <denemehesap8w8w@gmail.com>
2025-06-13 17:38:53 +0300

    Chakra UI importları tamamen kaldırıldı

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  <React.StrictMode>
     <App />
-  </ChakraProvider>
-); 
+  </React.StrictMode>
+);
+
+reportWebVitals(); 
